a6b74b2105ab4725cc2b7287f2abb150d5058d7d
nksoluna <61101110286@psu.ac.th>
2021-05-07 17:38:08 +0700

    Listview widget

--- lib/main.dart
+++ lib/main.dart
@@ -22,39 +22,59 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   int counter = 0;
+ 
 
   @override
   Widget build(BuildContext context) {
+     
+  
+
     return Scaffold(
       appBar: AppBar(title: Text("KEEARAIDIN")),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image(
-                image: NetworkImage(
-                    'https://png.pngtree.com/png-clipart/20190515/original/pngtree-vector-cartoon-illustration-of-a-traditional-set-of-fast-food-me-png-image_3565698.jpg')),
-            Text("WELCOME TO KEEARAIDIN"),
-            Text("กินอะไรดี ....."),
-            Text("สั่งอาหาร คลิกที่ปุ่มนี้"),
-            Text(
-              "สั่งอาหารไปแล้วทั้งหมด $counter ครั้ง ",
-              style: TextStyle(fontSize: 30),
-            ),
-          ],
+        child: ListView(
+        
+          children: getData(counter) ,
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
+        onPressed:  addNumber,
+        child : Icon(Icons.add_shopping_cart),
+      ),
+    );
+  
+}
+
+void addNumber() {
+      setState(() {
             counter++;
             if (counter >=20) {
               counter = 0 ;
             }
           });
-        },
-        child : Icon(Icons.add_shopping_cart),
-      ),
-    );
-  }
+    }
+}
+
+List<Widget> getData(int number) {
+  List<Widget> data = [] ;
+  data.add(Image(
+                image: NetworkImage(
+                    'https://png.pngtree.com/png-clipart/20190515/original/pngtree-vector-cartoon-illustration-of-a-traditional-set-of-fast-food-me-png-image_3565698.jpg')));
+  
+  
+  data.add(Text("WELCOME TO KEEARAIDIN" ,
+  style: TextStyle(fontSize: 30), ));
+  data.add(Text("กินอะไรดี ....." ,
+  style: TextStyle(fontSize: 30), ));
+  data.add(Text("สั่งอาหาร คลิกที่ปุ่มนี้" ,
+  style: TextStyle(fontSize: 30), ));
+  data.add(Text(
+              "สั่งอาหารไปแล้วทั้งหมด $number ครั้ง ",
+              style: TextStyle(fontSize: 30), ));
+              for (int i = 0 ; i<number ; i++) {
+                data.add(Text(
+              "รายการที่ $i ",
+              style: TextStyle(fontSize: 16), ));
+              }
+              return data ;
 }

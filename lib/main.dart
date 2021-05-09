076035ec55feaf32cca723a8d5beb729c5e3d53f
nksoluna <61101110286@psu.ac.th>
2021-05-09 16:38:08 +0700

    Update Menu

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'Foodmenu.dart' ;
 
 void main() {
   runApp(LabelOrtext());
@@ -21,60 +22,64 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<FoodMenu> menu = [
+    FoodMenu("กะเพราไก่ไข่ดาว" , "50"),
+    FoodMenu("ผัดพริกไก่ไข่ดาว" , "50"),
+     FoodMenu("ส้มตำไทย" , "40"),
+  ];
   int counter = 0;
- 
 
   @override
   Widget build(BuildContext context) {
-     
-  
-
     return Scaffold(
-      appBar: AppBar(title: Text("KEEARAIDIN")),
-      body: Center(
-        child: ListView(
-        
-          children: getData(counter) ,
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed:  addNumber,
-        child : Icon(Icons.add_shopping_cart),
-      ),
-    );
-  
-}
+      appBar: AppBar(title: Text("เมนูอาหารทั้งหมดที่ต้องการรับประทาน")),
+      body: ListView.builder(
+        itemCount: menu.length ,
+        itemBuilder: (BuildContext context , int indexmenu) {
+          FoodMenu foods = menu[indexmenu] ;
+        return ListTile(title: Text("เมนูที่ ${indexmenu+1}"),subtitle: Text(foods.name+" ราคา "+ foods.price+" บาท"),
+        );
+      }));
+  }
 
-void addNumber() {
-      setState(() {
-            counter++;
-            if (counter >=20) {
-              counter = 0 ;
-            }
-          });
-    }
+  void addNumber() {
+    setState(() {
+      counter++;
+      if (counter >= 20) {
+        counter = 0;
+      }
+    });
+  }
 }
 
 List<Widget> getData(int number) {
-  List<Widget> data = [] ;
+  List<Widget> data = [];
   data.add(Image(
-                image: NetworkImage(
-                    'https://png.pngtree.com/png-clipart/20190515/original/pngtree-vector-cartoon-illustration-of-a-traditional-set-of-fast-food-me-png-image_3565698.jpg')));
-  
-  
-  data.add(Text("WELCOME TO KEEARAIDIN" ,
-  style: TextStyle(fontSize: 30), ));
-  data.add(Text("กินอะไรดี ....." ,
-  style: TextStyle(fontSize: 30), ));
-  data.add(Text("สั่งอาหาร คลิกที่ปุ่มนี้" ,
-  style: TextStyle(fontSize: 30), ));
+      image: NetworkImage(
+          'https://png.pngtree.com/png-clipart/20190515/original/pngtree-vector-cartoon-illustration-of-a-traditional-set-of-fast-food-me-png-image_3565698.jpg')));
+
   data.add(Text(
-              "สั่งอาหารไปแล้วทั้งหมด $number ครั้ง ",
-              style: TextStyle(fontSize: 30), ));
-              for (int i = 0 ; i<number ; i++) {
-                data.add(Text(
-              "รายการที่ $i ",
-              style: TextStyle(fontSize: 16), ));
-              }
-              return data ;
+    "WELCOME TO KEEARAIDIN",
+    style: TextStyle(fontSize: 30),
+  ));
+  data.add(Text(
+    "กินอะไรดี .....",
+    style: TextStyle(fontSize: 30),
+  ));
+  data.add(Text(
+    "สั่งอาหาร คลิกที่ปุ่มนี้",
+    style: TextStyle(fontSize: 30),
+  ));
+  data.add(Text(
+    "สั่งอาหารไปแล้วทั้งหมด $number ครั้ง ",
+    style: TextStyle(fontSize: 30),
+  ));
+  for (int i = 0; i < number; i++) {
+    var menu = ListTile(
+      title: Text("เมนูที่ ${i + 1 }"),
+      subtitle: Text("ชื่อเมนู : "),
+    );
+    data.add(menu);
+  }
+  return data;
 }

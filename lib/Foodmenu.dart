693d4600ee987d083229743da57b1da5538172cc
nksoluna <61101110286@psu.ac.th>
2021-05-10 16:43:07 +0700

    Sizedbox

--- lib/Foodmenu.dart
+++ lib/Foodmenu.dart
@@ -1,6 +1,8 @@
 class FoodMenu { 
   String name ;
   String price ;
+  String image ;
 
-  FoodMenu(this.name , this.price) ; // name bf price 
+  FoodMenu(this.name , this.price , this.image) ; // name bf price
+  
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'Foodmenu.dart' ;
+import 'Foodmenu.dart';
 
 void main() {
   runApp(LabelOrtext());
@@ -23,23 +23,143 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   List<FoodMenu> menu = [
-    FoodMenu("กะเพราไก่ไข่ดาว" , "50"),
-    FoodMenu("ผัดพริกไก่ไข่ดาว" , "50"),
-     FoodMenu("ส้มตำไทย" , "40"),
+    FoodMenu("กะเพราไก่ไข่ดาว", "50", "assets/image/krapow.jpg"),
+    FoodMenu("ผัดพริกไก่ไข่ดาว", "50", "assets/image/padprick.jpg"),
+    FoodMenu("ส้มตำไทย", "40", "assets/image/papayasalad.jpg"),
+    FoodMenu("ผัดไทย", "45", "assets/image/padthai.jpg")
   ];
   int counter = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("เมนูอาหารทั้งหมดที่ต้องการรับประทาน")),
-      body: ListView.builder(
-        itemCount: menu.length ,
-        itemBuilder: (BuildContext context , int indexmenu) {
-          FoodMenu foods = menu[indexmenu] ;
-        return ListTile(title: Text("เมนูที่ ${indexmenu+1}"),subtitle: Text(foods.name+" ราคา "+ foods.price+" บาท"),
-        );
-      }));
+        appBar: AppBar(
+            title: Text(
+          "บัญชีของฉัน",
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        )),
+        body: Padding(
+          padding: const EdgeInsets.fromLTRB(10,10,10, 0), // space from top and side
+          child: Column(
+            children: [
+              Container(
+                  padding: const EdgeInsets.all(40.0), // you can use s
+                  decoration: BoxDecoration(
+                      color: Colors.orange,
+                      borderRadius: BorderRadius.circular(10)),
+                  height: 150,
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        "ยอดคงเหลือ",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      Expanded(
+                        child: Text(
+                          " 15000 บาท",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold),
+                          textAlign: TextAlign
+                              .right, // outside style and for aligning text
+                        ),
+                      )
+                    ],
+                  )),
+                  SizedBox(height: 10),
+                 Container(
+                    padding: const EdgeInsets.all(40.0),
+                    decoration: BoxDecoration(
+                        color: Colors.greenAccent[400],
+                        borderRadius: BorderRadius.circular(10)),
+                    height: 120,
+                    child: Row(
+                      children: [
+                        Text(
+                          "รายรับ",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        Expanded(
+                          child: Text(
+                            " 25000 บาท",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 25,
+                                fontWeight: FontWeight.bold),
+                                textAlign: TextAlign.right,
+                          ),
+                        )
+                      ],
+                    )),
+              
+            SizedBox(height: 10),
+                Container(
+                    padding: const EdgeInsets.all(40.0),
+                    decoration: BoxDecoration(
+                        color: Colors.redAccent[700],
+                        borderRadius: BorderRadius.circular(10)),
+                    height: 120,
+                    child: Row(
+                      children: [
+                        Text(
+                          "ค้างชำระ",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        Expanded(
+                          child: Text(
+                            " 0 บาท",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 25,
+                                fontWeight: FontWeight.bold),
+                                textAlign: TextAlign.right,
+                          ),
+                        )
+                      ],
+                    )),
+              SizedBox(height: 10), //define transparent box to cut space from container
+                Container(
+                    padding: const EdgeInsets.all(40.0),
+                    decoration: BoxDecoration(
+                        color: Colors.deepOrangeAccent[400],
+                        borderRadius: BorderRadius.circular(10)),
+                    height: 120,
+                    child: Row(
+                      children: [
+                        Text(
+                          "รายจ่าย",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        Expanded(
+                          child: Text(
+                            " 10000 บาท",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 25,
+                                fontWeight: FontWeight.bold),
+                                textAlign: TextAlign.right,
+                          ),
+                        )
+                      ],
+                    )),
+              
+            ],
+          ),
+        ));
   }
 
   void addNumber() {
@@ -52,7 +172,9 @@ class _HomePageState extends State<HomePage> {
   }
 }
 
-List<Widget> getData(int number) {
+
+//Increasement of data and counter 
+/*List<Widget> getData(int number) {
   List<Widget> data = [];
   data.add(Image(
       image: NetworkImage(
@@ -76,10 +198,11 @@ List<Widget> getData(int number) {
   ));
   for (int i = 0; i < number; i++) {
     var menu = ListTile(
-      title: Text("เมนูที่ ${i + 1 }"),
+      title: Text("เมนูที่ ${i + 1}"),
       subtitle: Text("ชื่อเมนู : "),
     );
     data.add(menu);
   }
   return data;
 }
+*/
